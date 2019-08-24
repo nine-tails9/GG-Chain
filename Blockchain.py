54fac57316d5c469e58e253aaa5f9ed7dbf03415
arvindpunk <arvindpunk@gmail.com>
2019-08-24 16:17:56 +0530

    fix

---
+++ Blockchain.py
@@ -0,0 +1,55 @@
+from hashlib import sha256
+import time
+import json
+
+class Block:
+	def __init__(self, index, parameters, timestamp, previousHash):
+		self.index = []
+		self.parameters = parameters
+		self.timestamp = timestamp
+		self.previousHash = previousHash
+		# self.hash
+
+	def computeHash(self):
+		blockString = json.dumps(self.__dict__, sort_keys=True)
+		return sha256(blockString.encode()).hexdigest()
+
+	def getAccuracy(self):
+		# TO-DO: create a accuracy solving function from test data
+		return 0
+
+
+class Blockchain:
+	threshold = 0.1
+
+	def __init__(self):
+		self.chain = []
+		self.createGenesisBlock()
+		self.currentAccuracy = self.getLastBlock().getAccuracy()
+
+	def createGenesisBlock(self):
+		genesisBlock = Block(0, [], time.time(),"0")
+		genesisBlock.hash = genesisBlock.computeHash()
+		self.chain.append(genesisBlock)
+
+	@property
+	def lastBlock(self):
+		return self.chain[-1]
+
+	def addBlock(self, block):
+		previousHash = self.lastBlock.hash
+
+		if previousHash != block.previousHash:
+			return False
+
+		if not self.isValidProof(block):
+			return False
+
+		self.chain.append(block)
+		self.currentAccuracy = block.getAccuracy()
+		return True
+
+	def isValidProof(self, block):
+		if block.getAccuracy() - self.currentAccuracy > threshold:
+			return True
+		return False
